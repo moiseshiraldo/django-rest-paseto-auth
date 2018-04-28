4a815a9b99f9c5b2ad9a5a5e646d662cf745b559
Moises Hiraldo <moiseshiraldo@gmail.com>
2018-04-28 20:37:14 +0100

    Return lazy user on auth scheme

--- paseto_auth/authentication.py
+++ paseto_auth/authentication.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils.functional import SimpleLazyObject
 
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import authentication
@@ -7,6 +9,30 @@ from rest_framework import authentication
 from .models import AppRefreshToken, AppIntegrationUser
 from .settings import AUTH_SETTINGS
 from .tokens import AccessToken
+
+
+def get_user(access_token):
+    """
+    Returns the user associated with the given access token.
+    If no user is retrieved, return an instance of `AnonymousUser`.
+    """
+    try:
+        if access_token.data['model'] == 'user':
+            user_model = get_user_model()
+            user = user_model.objects.get(
+                pk=access_token.data['pk'],
+                is_active=True,
+            )
+        elif access_token.data['model'] == 'app':
+            app_token = AppRefreshToken.objects.get(
+                pk=access_token.data['pk'],
+                locked=False
+            )
+            user = AppIntegrationUser(app_token)
+    except ObjectDoesNotExist:
+        user = AnonymousUser()
+
+    return user
 
 
 class PasetoAuthentication(authentication.BaseAuthentication):
@@ -32,23 +58,10 @@ class PasetoAuthentication(authentication.BaseAuthentication):
             return None
 
         access_token = AccessToken(token=header[1])
+        
         if not access_token.is_valid():
             raise AuthenticationFailed("Invalid access token")
 
-        try:
-            if access_token.data['model'] == 'user':
-                user_model = get_user_model()
-                user = user_model.objects.get(
-                    pk=access_token.data['pk'],
-                    is_active=True,
-                )
-            elif access_token.data['model'] == 'app':
-                app_token = AppRefreshToken.objects.get(
-                    pk=access_token.data['pk'],
-                    locked=False
-                )
-                user = AppIntegrationUser(app_token)
-        except ObjectDoesNotExist:
-            raise AuthenticationFailed("Invalid access token")
+        user = SimpleLazyObject(lambda: get_user(access_token))
 
         return (user, access_token)
