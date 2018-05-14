78df4ab3c432d30ed560ec01e796a3e25d032efb
Moises Hiraldo <moiseshiraldo@gmail.com>
2018-05-14 20:10:45 +0100

    Release version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
   name='django-rest-paseto-auth',
-  version='0.1.0',
+  version='0.1.1',
   description='Paseto authentication for Django Rest Framework',
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
